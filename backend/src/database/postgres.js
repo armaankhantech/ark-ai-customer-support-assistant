5c2b744154995e1eb87d73b2b7749c7538d77b19
Armaan Khan <armaankhantech@gmail.com>
2026-08-08 19:44:50 +0530

    clean postgres startup logs

--- backend/src/database/postgres.js
+++ backend/src/database/postgres.js
@@ -1,18 +1,11 @@
+const { Pool } = require("pg");
+const env = require("../config/env");
 
-    const { Pool } = require("pg");
-    const env = require("../config/env");
+const pool = new Pool({
+    host: env.POSTGRES_HOST,
+    user: env.POSTGRES_USER,
+    password: env.POSTGRES_PASSWORD,
+    database: env.POSTGRES_DB,
+});
 
-    const pool = new Pool({
-        host: env.POSTGRES_HOST,
-        user: env.POSTGRES_USER,
-        password: env.POSTGRES_PASSWORD,
-        database: env.POSTGRES_DB,
-    });
-
-    pool.query("SELECT current_database(), current_user, inet_server_addr(), inet_server_port()")
-.then(res => {
-    console.log("CONNECTED DATABASE:");
-    console.table(res.rows);
-})
-.catch(console.error);
-    module.exports = pool;
+module.exports = pool;
